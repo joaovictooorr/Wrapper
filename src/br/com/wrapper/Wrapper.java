ba3763972fbdad24a0f8ac63c7591571ea76c930
joaovictooorr <joaovitorwf01020304@gmail.com>
2024-03-05 17:44:31 -0300

    Wrapper

---
+++ src/br/com/wrapper/Wrapper.java
@@ -0,0 +1,19 @@
+package br.com.wrapper;
+
+import java.util.Scanner;
+
+public class Wrapper {
+
+    public static void wrapper(){
+        Scanner input = new Scanner(System.in);
+        long num = input.nextLong();
+        num = Long.valueOf(num);
+        System.out.println(num);
+    }
+
+    public static void main(String[] args){
+
+        wrapper();
+
+    }
+}
