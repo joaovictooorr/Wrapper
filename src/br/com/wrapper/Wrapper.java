e2f7c4b63fda0134938a2ffe59cdfc0f0ec88818
joaovictooorr <joaovitorwf01020304@gmail.com>
2024-03-05 18:02:11 -0300

    ajustando codigo

--- src/br/com/wrapper/Wrapper.java
+++ src/br/com/wrapper/Wrapper.java
@@ -7,7 +7,7 @@ public class Wrapper {
     public static void wrapper(){
         Scanner input = new Scanner(System.in);
         long num = input.nextLong();
-        num = Long.valueOf(num);
+        Long num2 = Long.valueOf(num);
         System.out.println(num);
     }
 
